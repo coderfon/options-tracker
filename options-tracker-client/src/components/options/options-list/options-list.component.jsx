e3c02cb7fd6d8158b9fc1da60de9b46bc7da5642
Alfonso Muñoz <alfonso.mg@outlook.com>
2022-09-28 20:56:44 +0200

    OptionsList filters

--- options-tracker-client/src/components/options/options-list/options-list.component.jsx
+++ options-tracker-client/src/components/options/options-list/options-list.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import DatePicker from "react-datepicker"
 import { DateToString } from "../../../utils/date-utils";
 import { ObjectListToCsv } from "../../../utils/csv-utils"
 
@@ -11,28 +12,94 @@ const OptionsList = () => {
 
     const optionsList = useSelector((state) => state.options.list);
     const optionsListReversed = [...optionsList].reverse().map(a => a);
-    const [checked, setChecked] = useState(true);
+    const [reverseList, setReverseList] = useState(true);
+    const [actionFilter, setActionFilter] = useState('ALL');
+    const [campaignFilter, setCampaignFilter] = useState('');
+    const [currencyFilter, setCurrencyFilter] = useState('');
+    const [tickerFilter, setTickerFilter] = useState('');
+    const [typeFilter, setTypeFilter] = useState('ALL');
+    const [fromDateFilter, setFromDateFilter] = useState();
+    const [toDateFilter, setToDateFilter] = useState();
+    const [fromExpirationFilter, setFromExpirationFilter] = useState();
+    const [toExpirationFilter, setToExpirationFilter] = useState();
+    
 
-    const optionsListToRender = checked ? optionsListReversed : optionsList; 
+    let today = new Date();
+    let optionsListToRender = reverseList ? optionsListReversed : optionsList;
+
+    if (actionFilter && actionFilter !== 'ALL') {
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.action === actionFilter;
+        });
+    }
+
+    if (campaignFilter) {
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.campaign.includes(campaignFilter);
+        });
+    }
+
+    if (currencyFilter) {
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.currency.includes(currencyFilter);
+        });
+    }
+
+    if (fromDateFilter) {
+        const calculatedDate = new Date(fromDateFilter);
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.date >= fromDateFilter.getTime();
+        });
+    }
+
+    if (tickerFilter) {
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.ticker.includes(tickerFilter);
+        });
+    }
+
+    if (typeFilter && typeFilter !== 'ALL') {
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.type === typeFilter;
+        });
+    }
+
+    if (toDateFilter) {
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.date <= toDateFilter.getTime();
+        });
+    }
+
+    if (fromExpirationFilter) {
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.expiration >= fromExpirationFilter.getTime();
+        });
+    }
+
+    if (toExpirationFilter) {
+        optionsListToRender = optionsListToRender.filter(option => {
+            return option.expiration <= toExpirationFilter.getTime();
+        });
+    }
 
     const exportCsv = (event) => {
-        
+
         console.log(optionsList);
 
         let csvContent = ObjectListToCsv(optionsList);
-        let fileName = new Date().toJSON().slice(0,16);
+        let fileName = new Date().toJSON().slice(0, 16);
 
         var encodedUri = encodeURI(csvContent);
         var link = document.createElement("a");
         link.setAttribute("href", encodedUri);
         link.setAttribute("download", `options-tracker-${fileName}.csv`);
         document.body.appendChild(link); // Required for FF
-        
+
         link.click(); // This will download the data file named "my_data.csv".
     }
 
-    const handleChange = () => {
-        setChecked(!checked);
+    const reverseCheckboxHandler = () => {
+        setReverseList(!reverseList);
     };
 
     return (
@@ -42,51 +109,129 @@ const OptionsList = () => {
             </div>
             <div>
                 <label>
-                <input
-                    type="checkbox"
-                    checked={checked}
-                    onChange={handleChange}/> Show in reverse
+                    <input
+                        type="checkbox"
+                        checked={reverseList}
+                        onChange={reverseCheckboxHandler} /> Show in reverse
                 </label>
             </div>
             <table>
                 <thead>
                     <tr>
-                        <th>date</th>
-                        <th>ticker</th>
-                        <th>type</th>
-                        <th>action</th>
+                        <th>
+                            <div>
+                                <DatePicker
+                                    dateFormat="yyyy-MM-dd"
+                                    className="listFilter"
+                                    placeholderText="From date"
+                                    selected={fromDateFilter}
+                                    selectsStart
+                                    startDate={fromDateFilter}
+                                    endDate={toDateFilter}
+                                    onChange={(date) =>
+                                        setFromDateFilter(date)} />
+                            </div>
+                            <div>
+                                <DatePicker dateFormat="yyyy-MM-dd"
+                                    className="listFilter"
+                                    placeholderText="To date"
+                                    selected={toDateFilter}
+                                    selectsEnd
+                                    startDate={fromDateFilter}
+                                    endDate={toDateFilter}
+                                    minDate={fromDateFilter}
+                                    onChange={(date) => setToDateFilter(date)} />
+                            </div>
+                        </th>
+                        <th>
+                            <input name="tickerFilter"
+                                type="text"
+                                className="listFilter"
+                                placeholder="ticker"
+                                onChange={e => setTickerFilter(e.target.value)} />
+                        </th>
+                        <th>
+                            <select name="type" value={typeFilter} onChange={e => setTypeFilter(e.target.value)}>
+                                <option value="ALL">C/P</option>
+                                <option value="PUT">PUT</option>
+                                <option value="CALL">CALL</option>
+                            </select>
+                        </th>
+                        <th>
+                            <select name="action" value={actionFilter} onChange={e => setActionFilter(e.target.value)}>
+                                <option value="ALL">b/s</option>
+                                <option value="buy">buy</option>
+                                <option value="sell">sell</option>
+                            </select>
+                        </th>
                         <th>strike</th>
                         <th>lastPrice</th>
-                        <th>expiration</th>
-                        <th>contracts</th>
-                        <th>contractSize</th>
+                        <th>
+                            <div>
+                                <DatePicker
+                                    dateFormat="yyyy-MM-dd"
+                                    className="listFilter"
+                                    placeholderText="From exp"
+                                    selected={fromExpirationFilter}
+                                    selectsStart
+                                    startDate={fromExpirationFilter}
+                                    endDate={toExpirationFilter}
+                                    onChange={(date) =>
+                                        setFromExpirationFilter(date)} />
+                            </div>
+                            <div>
+                                <DatePicker dateFormat="yyyy-MM-dd"
+                                    className="listFilter"
+                                    placeholderText="To exp"
+                                    selected={toExpirationFilter}
+                                    selectsEnd
+                                    startDate={fromExpirationFilter}
+                                    endDate={toExpirationFilter}
+                                    minDate={fromExpirationFilter}
+                                    onChange={(date) => setToExpirationFilter(date)} />
+                            </div>
+                        </th>
+                        <th>ctrs</th>
+                        <th>ctrsSz</th>
                         <th>premium</th>
-                        <th>currency</th>
-                        <th>conversionRate</th>
-                        <th>commission</th>
-                        <th>campaign</th>
+                        <th>
+                            <input name="currencyFilter"
+                                type="text"
+                                className="listFilter"
+                                placeholder="currency"
+                                onChange={e => setCurrencyFilter(e.target.value)} />
+                        </th>
+                        <th>conv.Rate</th>
+                        <th>comm</th>
+                        <th>
+                            <input name="campaignFilter"
+                                type="text"
+                                className="listFilter"
+                                placeholder="campaign"
+                                onChange={e => setCampaignFilter(e.target.value)} />
+                        </th>
                         <th>actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                {optionsListToRender.map((option) => (
-                    <tr key={option.id}>
-                        <td>{DateToString(new Date(option.date))}</td>
-                        <td>{option.ticker}</td>
-                        <td>{option.type}</td>
-                        <td>{option.action}</td>
-                        <td>{option.strike}</td>
-                        <td>{option.lastPrice}</td>
-                        <td>{DateToString(new Date(option.expiration))}</td>
-                        <td>{option.contracts}</td>
-                        <td>{option.contractSize}</td>
-                        <td>{option.premium}</td>
-                        <td>{option.currency}</td>
-                        <td>{option.conversionRate}</td>
-                        <td>{option.commission}</td>
-                        <td>{option.campaign}</td>
-                        <td><Link to={`/option/edit/${option.id}`}>Edit</Link></td>
-                    </tr>
+                    {optionsListToRender.map((option) => (
+                        <tr key={option.id} className={option.expiration >= today.getTime() ? 'active' : 'expired'}>
+                            <td>{DateToString(new Date(option.date))}</td>
+                            <td>{option.ticker}</td>
+                            <td>{option.type}</td>
+                            <td>{option.action}</td>
+                            <td>{option.strike}</td>
+                            <td>{option.lastPrice}</td>
+                            <td>{DateToString(new Date(option.expiration))}</td>
+                            <td>{option.contracts}</td>
+                            <td>{option.contractSize}</td>
+                            <td>{option.action === "sell" ? option.premium : `[${option.premium}]`}</td>
+                            <td>{option.currency}</td>
+                            <td>{option.conversionRate}</td>
+                            <td>{option.commission}</td>
+                            <td>{option.campaign}</td>
+                            <td><Link to={`/option/edit/${option.id}`}>Edit</Link></td>
+                        </tr>
                     ))}
                 </tbody>
             </table>
--- options-tracker-client/src/utils/date-utils.js
+++ options-tracker-client/src/utils/date-utils.js
@@ -1,3 +1,9 @@
+export const AddDays = (date, days) => {
+    let originalTimeStamp = date.getTime();
+    originalTimeStamp += days * 86400000;
+    return new Date(originalTimeStamp);
+}
+
 export const DateToString = (date) => {
     const year = date.toLocaleString("default", { year: "numeric" });
     const month = date.toLocaleString("default", { month: "2-digit" });
